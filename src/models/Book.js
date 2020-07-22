665a2c10294cfbe2f7a24c32eada303150a0d941
shahreaz0 <ashahreaz@gmail.com>
2020-07-23 01:44:10 +0600

    Books route with search capability

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,30 +1,33 @@
 const mongoose = require("mongoose");
 
-const bookSchema = mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		description: {
-			type: String,
-			trim: true,
-		},
-		coverImagePath: {
-			type: String,
-			required: true,
-		},
-		pageNo: {
-			type: Number,
-		},
-		language: {
-			type: String,
-		},
+const bookSchema = mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+		trim: true,
 	},
-	{
-		timestamps: true,
+	author: {
+		type: String,
+		required: true,
 	},
-);
+	description: {
+		type: String,
+		trim: true,
+	},
+	coverImagePath: {
+		type: String,
+		required: true,
+	},
+	pageNo: {
+		type: Number,
+	},
+	language: {
+		type: String,
+	},
+	publishDate: {
+		type: Date,
+		default: new Date(),
+	},
+});
 
 module.exports = mongoose.model("Book", bookSchema);
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,7 +1,56 @@
 const router = require("express").Router();
 const Book = require("../models/Book");
 
+// const book = new Book({
+// 	title: "New world with islam 2",
+// 	description: "new description",
+// 	coverImagePath:
+// 		"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+// 	pageNo: 10,
+// 	language: "english",
+// 	author: "maria",
+// });
+
+// book.save()
+// 	.then((book) => console.log(book))
+// 	.catch((error) => console.log(error));
+
 // GET --> /books --> Shows All books
+router.get("/books", async (req, res) => {
+	try {
+		// search by options
+		let query = Book.find();
+		if (req.query.title) {
+			query = query.where("title").regex(new RegExp(req.query.title, "i"));
+		}
+		if (req.query.author) {
+			query = query.where("author").regex(new RegExp(req.query.author, "i"));
+		}
+		if (req.query.publishAfter) {
+			query = query.where("publishDate").gte(req.query.publishAfter);
+		}
+		if (req.query.publishBefore) {
+			query = query.where("publishDate").lte(req.query.publishBefore);
+		}
+		if (req.query.language) {
+			query = query.where("language").equals(req.query.language);
+		}
+
+		// find
+		const books = await query.exec();
+
+		// render
+		res.render("books/index", {
+			pageTitle: "Books",
+			path: req.path,
+			searchOptions: req.query,
+			books,
+		});
+	} catch (error) {
+		res.render("error", { backButton: "/books", error });
+	}
+});
+
 // POST --> /books --> Create books
 // GET --> /books/new --> Show create book form
 // GET --> /books/new --> Show individual book in details
