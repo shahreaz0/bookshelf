9bb55bd4e09c69d40f3cc48151cd5f79d7c61917
shahreaz0 <ashahreaz@gmail.com>
2020-08-29 21:45:43 +0600

    In user profile, edit/delete/download dynamic button

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -6,31 +6,22 @@ const User = require("../models/User");
 //middleware
 const { isLoggedIn } = require("../configs/middleware");
 
-router.get("/profile/", isLoggedIn, async (req, res) => {
+router.get("/profile/:id", async (req, res) => {
 	try {
-		const user = await User.findById(req.user._id).populate("posts").exec();
-		res.render("profile/index", { pageTitle: "Profile", user });
+		const user = await User.findById(req.params.id).populate("posts").exec();
+
+		res.render("profile/index", { pageTitle: "Profile", userProfile: user });
 	} catch (error) {
 		console.log("profile.js line 13", error);
 		res.send({ error });
 	}
 });
 
-router.get("/profile/:id", isLoggedIn, async (req, res) => {
-	// try {
-	// 	const user = await User.findById(req.user._id).populate("posts").exec();
-	// 	res.render("profile/index", { pageTitle: "Profile", user });
-	// } catch (error) {
-	// 	console.log("profile.js line 13", error);
-	// 	res.send({ error });
-	// }
-});
-
-router.put("/profile/edit", isLoggedIn, async (req, res) => {
+router.put("/profile/:id/edit", isLoggedIn, async (req, res) => {
 	res.send("edit profile");
 });
 
-router.delete("/profile", isLoggedIn, async (req, res) => {
+router.delete("/profile/:id", isLoggedIn, async (req, res) => {
 	res.send("delete user account");
 });
 
