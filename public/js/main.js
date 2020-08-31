61e037f3c0b221f173d7d39609c71fbd3ebd001e
shahreaz0 <ashahreaz@gmail.com>
2020-08-31 06:29:50 +0600

    Some ui fixed and tooltip data simplify

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,12 @@ new FroalaEditor("textarea#add-book-textarea");
 new FroalaEditor("textarea#edit-book-textarea");
 
 // tippy.js
-const length = document.getElementById("length").dataset.length;
+tippy("#tooltip", {});
+// for (let i = 0; i < length; i++) {
+// 	const name = `tooltip${i}`;
+// 	const content = document.getElementById(name)?.dataset.tooltip;
 
-for (let i = 0; i < length; i++) {
-	const name = `tooltip${i}`;
-	const content = document.getElementById(name)?.dataset.tooltip;
-
-	tippy(`#${name}`, {
-		content: content,
-	});
-}
+// 	tippy(`#${name}`, {
+// 		content: content,
+// 	});
+// }
